ff024c557105e0d02aaf0a3efb5dd38199873443
radapunvich <rada.pan@student.mahidol.edu>
2026-03-30 08:36:30 +0000

    ueh

--- monster.cpp
+++ monster.cpp
@@ -1,6 +1,22 @@
-//
-//  monster.cpp
-//  monster
-//
-//  Created by Mingmanas Sivaraksa on 30/3/2566 BE.
-//
+#include <iostream>
+using namespace std;
+#include "monster.h"
+
+int main(int argc, const char * argv[]) {
+    monster m1("Noppo", 10, 1);
+    monster m2("Bonny"); 
+    monster m4("Alice", 20, 2); 
+    monster *p;
+    
+    monster x[5] = {
+        {"Hakao", 5, 1}, 
+        {"Chicken", 10, 20},
+        {"Duck", 0, 0},
+        {"Goat", 0, 0},
+        {"Cow", 0, 0}
+    };
+    p=new monster("Lulu", 10, 50);
+    delete p;//destructor p
+    
+    return 0;//destructor of x, m4,m2,m1
+}
--- monster.h
+++ monster.h
@@ -1,21 +1,31 @@
-//
-//  monster.h
-//  monster
-//
-//  Created by Mingmanas Sivaraksa on 30/3/2566 BE.
-//
-
 #ifndef monster_h
 #define monster_h
-class monster{
+#include <iostream>
+#include <string>
+using namespace std;
+
+class monster {
 private:
     string name;
-    int hp,potion;
+    int hp, potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string n = "Balky", int h = 100, int p = 0);
+    ~monster();
 };
+
+monster::~monster(){
+    cout<<"Bye ja"<<name<<"hp: "<<hp<<endl;
+}
+monster::monster(string n, int h, int p) {
+    name = n;
+    h > 200 ? hp = 200 : hp = h;
+    p > 10 ? potion = 10 : potion = p;
+
+    cout << "Monster " << name << " mlem" << endl;
+}
+
 
 
 #endif /* monster_h */
